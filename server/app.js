fd2adf223413eda04531fc9e00543a9cb1270013
Alan Tang <atang8013@gmail.com>
2019-09-17 13:45:40 -0400

    functioning with dummy data -- mongoDB setup

--- server/app.js
+++ server/app.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use("/graphql", graphqlHTTP({}));
+//connect tomlab database
+mongoose.connect(
+  "mongodb+srv://alan:mongotest@cluster0-221yf.mongodb.net/test?retryWrites=true&w=majority"
+);
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to Mongo DB");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(4000, () => {
   console.log("Now listening on port 4000");
